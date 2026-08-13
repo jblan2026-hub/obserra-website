3fba1abd7811483cc64a243198f58548d07a2d45
jblan2026-hub <jblan2026@gmail.com>
2026-08-13 01:31:41 -0400

    Add make-up administration page shell

---
+++ app/florida-security-training/admin/makeup/page.tsx
@@ -0,0 +1,3 @@
+export default function FloridaClassDMakeupAdminPage() {
+  return <main><h1>Class D Make-Up Administration</h1><p>Controlled assignment and time reconciliation workspace.</p></main>;
+}
